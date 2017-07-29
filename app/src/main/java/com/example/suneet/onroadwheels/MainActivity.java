cf29eeb3ac83eee10bbcc3c2151c65709c63fb9d
codedsun <suneetbond91@gmail.com>
2017-07-29 12:05:10 +0530

    Updated for slot 1:12:00PM

--- app/src/main/java/com/example/suneet/onroadwheels/MainActivity.java
+++ app/src/main/java/com/example/suneet/onroadwheels/MainActivity.java
@@ -1,17 +1,43 @@
 package com.example.suneet.onroadwheels;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-
-
+    EditText mobileNo;
+    Button goButton;
+    String mobNo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mobileNo= (EditText) findViewById(R.id.mobileNo);
+        goButton= (Button) findViewById(R.id.buttonGo);
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!((mobileNo.getText().toString().length())==0)) {
+
+                    mobNo = mobileNo.getText().toString();
+                    Intent i=new Intent(MainActivity.this,Registration.class);
+                    i.putExtra("MobileNo",mobNo);
+                    startActivity(i);
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this,"Enter the Mobile No",Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/suneet/onroadwheels/Registration.java
+++ app/src/main/java/com/example/suneet/onroadwheels/Registration.java
@@ -1,14 +1,59 @@
 package com.example.suneet.onroadwheels;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class Registration extends Activity {
+    Intent recieved;
+    String mobNo;
+    ImageView profilePic;
+    TextView mobileNo;
+    EditText username;
+    RadioButton genderM;
+    RadioButton genderF;
+    EditText emergencyNo;
+    EditText drivingLicenceNo;
+    Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        profilePic= (ImageView) findViewById(R.id.userImage);
+        mobileNo= (TextView) findViewById(R.id.userMobNo);
+        username= (EditText) findViewById(R.id.userName);
+        genderF= (RadioButton) findViewById(R.id.uFemale);
+        genderM= (RadioButton) findViewById(R.id.uMale);
+        emergencyNo= (EditText) findViewById(R.id.uEmergencyNo);
+        drivingLicenceNo= (EditText) findViewById(R.id.uDLNO);
+        nextButton= (Button) findViewById(R.id.buttonNext);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Registration.this,VehicalRegisteration.class);
+                startActivity(i);
+            }
+        });
+        recieved=getIntent();
+        try {
+            mobNo = recieved.getStringExtra("MobileNo");
+            Log.e("TAG", "onCreate: "+mobNo );
+            mobileNo.setText(mobNo+"");
+
+        }
+        catch (Exception e)
+        {
+            Log.e("TAG", "onCreate:registartion Exception"+e.getMessage());
+        }
+
     }
 }
