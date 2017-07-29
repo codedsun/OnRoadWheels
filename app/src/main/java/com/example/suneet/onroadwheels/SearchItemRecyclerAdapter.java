241535dad539c3037c7ce066190c54a8621d8157
codedsun <suneetbond91@gmail.com>
2017-07-29 16:07:01 +0530

    Files And Layout Updated

---
+++ app/src/main/java/com/example/suneet/onroadwheels/SearchItemRecyclerAdapter.java
@@ -0,0 +1,44 @@
+package com.example.suneet.onroadwheels;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by suneet on 29/7/17.
+ */
+
+/*
+public class SearchItemRecyclerAdapter extends RecyclerView.Adapter<SearchItemRecyclerAdapter.ViewHolder> {
+    ArrayList<EmergencyContact> contactArrayList;
+    Context c;
+
+    public SearchItemRecyclerAdapter(ArrayList<EmergencyContact> contactArrayList, Context c) {
+        this.contactArrayList = contactArrayList;
+        this.c = c;
+    }
+
+    @Override
+    public SearchItemRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(c).inflate(R.layout.search_screen_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(SearchItemRecyclerAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
+*/
