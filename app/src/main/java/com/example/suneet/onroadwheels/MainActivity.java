f477f80b696f38bf24799ccc57d9843f9bfdbb32
codedsun <suneetbond91@gmail.com>
2017-07-30 10:08:26 +0530

    Firebase auth fixed

--- app/src/main/java/com/example/suneet/onroadwheels/MainActivity.java
+++ app/src/main/java/com/example/suneet/onroadwheels/MainActivity.java
@@ -4,10 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import android.text.TextUtils;
 
 import android.util.Log;
 
@@ -16,12 +13,15 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.database.DatabaseReference;
@@ -37,19 +37,30 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
     private EditText mobileNo;
     private Button goButton;
     private String mobNo;
+    private String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+    private Button mainNext;
+    LinearLayout linearLayoutMain2;
+    EditText mainVerificationCode;
     FirebaseDatabase firebaseDatabase;
+    FirebaseAuth mAuth;
     DatabaseReference databaseReference;
     PhoneAuthProvider phoneAuthProvider;
-    PhoneAuthProvider.OnVerificationStateChangedCallbacks callback;
+    PhoneAuthCredential credential;
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mcallback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         firebaseDatabase=FirebaseDatabase.getInstance();
+        mAuth=FirebaseAuth.getInstance();
         databaseReference=firebaseDatabase.getReference();
         phoneAuthProvider=PhoneAuthProvider.getInstance();
         mobileNo= (EditText) findViewById(R.id.mobileNo);
         goButton= (Button) findViewById(R.id.buttonGo);
+        linearLayoutMain2= (LinearLayout) findViewById(R.id.linearLayoutMain2);
+        mainVerificationCode= (EditText) findViewById(R.id.mainVerificationCode);
+        mainNext= (Button) findViewById(R.id.mainButtonNext);
 
         Spinner loginspinner = (Spinner)findViewById(R.id.loginspinner);
         loginspinner.setOnItemSelectedListener(this);
@@ -69,12 +80,9 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
                 int len = mobileNo.getText().toString().length();
                 if(len>=10&&len<=12) {
                     mobNo = mobileNo.getText().toString();
-
                     Log.e("TAG", "onClick: "+mobNo );
-                   phoneAuthProvider.verifyPhoneNumber(mobNo, 60, TimeUnit.SECONDS, MainActivity.this, callback);
-                    Intent i=new Intent(MainActivity.this,Registration.class);
-                    i.putExtra("MobileNo",mobNo);
-                    startActivity(i);
+                   phoneAuthProvider.verifyPhoneNumber(mobNo, 60, TimeUnit.SECONDS, MainActivity.this, mcallback);
+
 
                 }
                 else
@@ -85,10 +93,12 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
                 }
             }
         });
-        callback=new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        mcallback =new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
             @Override
             public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
-                Toast.makeText(MainActivity.this, "Done"+phoneAuthCredential, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Done"+phoneAuthCredential.getSmsCode(), Toast.LENGTH_SHORT).show();
+
+                userSignIn(phoneAuthCredential);
             }
 
             @Override
@@ -97,18 +107,63 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
             }
 
             @Override
+            public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                Log.e("TAG", "onCodeSent: Doosre ke phone number pe"+s);
+                mVerificationId=s;
+                mResendToken=forceResendingToken;
+                linearLayoutMain2.setVisibility(View.GONE);
+                mainVerificationCode.setVisibility(View.VISIBLE);
+                goButton.setVisibility(View.GONE);
+                mainNext.setVisibility(View.VISIBLE);
+
+                //super.onCodeSent(s, forceResendingToken);
+            }
+
+            @Override
             public void onCodeAutoRetrievalTimeOut(String s) {
                 super.onCodeAutoRetrievalTimeOut(s);
             }
 
+
+        };
+        mainNext.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-                Log.e("", "onCodeSent: "+ s +" TOken"+forceResendingToken.toString()) ;
-                super.onCodeSent(s, forceResendingToken);
+            public void onClick(View v) {
+                if(mainVerificationCode.getText().toString()!=null)
+                {
+                    credential=PhoneAuthProvider.getCredential(mVerificationId,mainVerificationCode.getText().toString());
+                    userSignIn(credential);
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Enter the Verification Code", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+
+    private void userSignIn(PhoneAuthCredential phoneAuthCredential){
+        mAuth.signInWithCredential(phoneAuthCredential).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(MainActivity.this, "Registration Successfull ", Toast.LENGTH_SHORT).show();
+                    Intent i=new Intent(MainActivity.this,Registration.class);
+                    i.putExtra("MobileNo",mobNo);
+                    startActivity(i);
+
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Registraion Unsuccessfull ", Toast.LENGTH_SHORT).show();
+                }
 
             }
-        };
-
+        });
     }
 
     @Override
